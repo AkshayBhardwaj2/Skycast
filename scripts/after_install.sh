23da338d4d17e1c57039a189848d111b722ed675
AkshayBhardwaj2 <akshay.bhardwaj2@globallogic.com>
2025-03-27 14:42:38 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,5 +2,16 @@
 echo 'run after_install.sh: ' >> /home/ec2-user/Skycast/deploy.log
 echo 'cd /home/ec2-user/Skycast' >> /home/ec2-user/Skycast/deploy.log
 cd /home/ec2-user/Skycast >> /home/ec2-user/Skycast/deploy.log
+
+# Clean up previous node_modules, package-lock.json, and npm cache
+echo 'Cleaning up node_modules, package-lock.json, and npm cache' >> /home/ec2-user/Skycast/deploy.log
+rm -rf node_modules package-lock.json >> /home/ec2-user/Skycast/deploy.log
+npm cache clean --force >> /home/ec2-user/Skycast/deploy.log
+
+# Run npm install
 echo 'npm install' >> /home/ec2-user/Skycast/deploy.log
 npm install >> /home/ec2-user/Skycast/deploy.log
+
+# Fix permissions
+echo 'Fixing permissions for node_modules' >> /home/ec2-user/Skycast/deploy.log
+chown -R ec2-user:ec2-user /home/ec2-user/Skycast/node_modules >> /home/ec2-user/Skycast/deploy.log
